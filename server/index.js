ee74d37c4ac7036bb27d1d53fad6bea0cf2817b9
khushi158 <khushibanchhor21@gmail.com>
2024-08-14 00:27:26 +0530

    khushi

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path=require("path");
 require("dotenv").config();
 app.use(cors());
+app.use(express.static(path.join(__dirname,"dist")))
+app.get("/",(req,res)=>{res.sendFile(path.join(__dirname,"dist/index.html"))})
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT;
